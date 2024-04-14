6de1f243306ffe75ad7f7b9d07443909dd699673
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-04-14 14:30:01 +0530

    imported the classes(DriverManager,SQLException) and interfaces(Connection,Statement,ResultSet) from sql package and Scanner class from util package

--- src/HospitalReservationSystem.java
+++ src/HospitalReservationSystem.java
@@ -1,3 +1,13 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.Scanner;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 public class HospitalReservationSystem {
+    public static void main(String[] args) {
+
+    }
 
 }
